9b21458f09a1f4cd395e5fd85ace534bc56f14e0
Michail Shaposhnikov <misha.shapo98@gmail.com>
2019-04-17 18:49:22 -0500

    moved question state up

--- frontend/src/components/SurveyPage/Survey/Question/index.js
+++ frontend/src/components/SurveyPage/Survey/Question/index.js
@@ -2,38 +2,16 @@ import React, {Component} from 'react';
 import Input from './Input.js';
 
 export default class Question extends Component {
- constructor(props) {
-   super(props)
-   this.state = {
-     response: this.props.value
-   }
- }
- componentDidMount(){
-   this.setState({
-     response: ""
-   })
- }
- componentWillUnmount(){
-   this.setState({
-     response: ""
-   })
- }
- handleChange = (event) => {
-   this.setState({
-     response: event.target.value
-   })
- }
  render() {
    return(
      <div style={{backgroundColor: 'red'}}>
       <h2>{this.props.title}</h2>
       {this.props.text}
       <br/>
-      <input id={this.props.title} name={this.props.title} onChange={this.handleChange} value={this.state.response}/>
-      // <Input handleChange={this.handleChange} title={this.props.title} value={this.props.value}/>
-      {this.state.response}
+      <input name={this.props.i} onChange={this.props.onChange}/>
+      {this.props.answer}
       <br/>
-      <button onClick={() => this.props.func(this.props.i, this.props.title, this.state.response)}>submit</button>
+      <button onClick={this.props.onSubmit}>submit</button>
      </div>
    )
  }
