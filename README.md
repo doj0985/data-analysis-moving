8c8b838d76aab9feb9dc6b30edb62d682f8a13e6
JZillifro <ZillifroJacob@gmail.com>
2019-04-24 23:24:25 -0500

    deploy please

--- README.md
+++
@@ -1,2 +0,0 @@
-# data-analysis-moving
-Finding the best place for you to live in Austin
---
+++ precache-manifest.dd8a22327b6c26d74580ea1a973b59b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "a7a09ff824d35cd0452e",
+    "url": "/static/css/main.5e7cce66.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c4bfa102c3dcba8fd9ce",
+    "url": "/static/js/2.0652f8ce.chunk.js"
+  },
+  {
+    "revision": "10ee34078434e3f87f8875ba9c8f42a1",
+    "url": "/static/media/family.10ee3407.png"
+  },
+  {
+    "revision": "8c2d7811d3e5c102aef286be637c37be",
+    "url": "/static/media/logo.8c2d7811.png"
+  },
+  {
+    "revision": "a7a09ff824d35cd0452e",
+    "url": "/static/js/main.ad3941a7.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "eeccca1017720dd7d47210efd38f5627",
+    "url": "/index.html"
+  }
+];
